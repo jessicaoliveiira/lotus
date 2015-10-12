3cfba3443bf14d6334e8ee616d5b1d9d7bfdde6b
RONYROCHA <rony.fox@hotmail.com>
2015-10-12 17:19:29 -0300

    produtos da categoria e home

---
+++ app/src/main/java/br/com/lotus_projeto_integrador/lotus/ProdCategoriaActivity.java
@@ -0,0 +1,20 @@
+package br.com.lotus_projeto_integrador.lotus;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ProdCategoriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prod_categoria);
+
+
+
+    }
+}
