d4ad6cf60fda5c86e6d2b308bf3c7686b67caff2
Gustavo <gustavo.rios0@gmail.com>
2015-10-21 21:29:17 -0200

    Intent Carrinho

--- app/src/main/java/br/com/lotus_projeto_integrador/lotus/CarrinhoFragment.java
+++ app/src/main/java/br/com/lotus_projeto_integrador/lotus/CarrinhoFragment.java
@@ -1,10 +1,12 @@
 package br.com.lotus_projeto_integrador.lotus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class CarrinhoFragment extends Fragment {
@@ -18,6 +20,22 @@ public class CarrinhoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_carrinho, container, false);
+        View view = inflater.inflate(R.layout.fragment_carrinho, container, false);
+
+        Button finalizarCompra = (Button) view.findViewById(R.id.finalizarCompra);
+        finalizarCompra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Pass the context and the Activity class you need to open from the Fragment Class, to the Intent
+                Intent intent = new Intent(getActivity(), PagamentoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        return view;
     }
+
+
 }
