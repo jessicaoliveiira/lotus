6ce5f8ba6472904701157f8ed4fa93c60e8ff892
CinthiaFerraz <rony.fox@hotmail.com>
2015-11-25 21:06:12 -0200

    View com webservice

--- app/src/main/java/br/com/lotus_projeto_integrador/lotus/CategoriaFragment.java
+++ app/src/main/java/br/com/lotus_projeto_integrador/lotus/CategoriaFragment.java
@@ -1,14 +1,30 @@
 package br.com.lotus_projeto_integrador.lotus;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class CategoriaFragment extends Fragment {
 
+    private ViewGroup container;
 
     public CategoriaFragment() {
         // Required empty public constructor
@@ -20,10 +36,83 @@ public class CategoriaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categoria, container, false);
+
+       View view = inflater.inflate(R.layout.fragment_categoria, container, false);
 
 
+        this.container = (ViewGroup) view.findViewById(R.id.container);
+
+        ConexaoWeb conexaoWeb = new ConexaoWeb();
+        conexaoWeb.execute();
+
+        return view;
+
+    }
+
+    private void addItem(int idCategoria, String nomeCategoria) {
+        CardView cardView = (CardView) LayoutInflater.from(getActivity()).inflate(R.layout.cardviewproduto, container, false);
 
 
+        TextView nome = (TextView) cardView.findViewById(R.id.nomeCategoria);
+
+
+        nome.setText(nomeCategoria);
+
+        container.addView(cardView);
+
+    }
+
+    public class ConexaoWeb extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+
+                URL url = new URL("http://tsitomcat.azurewebsites.net/julietg1/rest/categoria");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+                StringBuilder responseStrBuilder = new StringBuilder();
+                String inputStr;
+
+                //Lê linha a linha a resposta e armazena no StringBuilder
+                while ((inputStr = reader.readLine()) != null) responseStrBuilder.append(inputStr);
+
+                String respostaCompleta = responseStrBuilder.toString();
+
+                Log.v("Json", respostaCompleta);
+
+                return respostaCompleta;
+
+            } catch (Exception e) {
+
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONArray json = new JSONArray(s);
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject jsonobject = json.getJSONObject(i);
+                    //JSONObject json = new JSONObject(s);
+
+                    int idCategoria = jsonobject.getInt("idCategoria");
+                    String nomeCategoria = jsonobject.getString("nomeCategoria");
+
+
+                    addItem(idCategoria, nomeCategoria);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
