aefe8ca7a96226e3f6d4c4c2b4046f137fb3d22a
Jessica <jekioliveira92@gmail.com>
2015-11-26 16:43:37 -0200

    correcoes do layout

--- app/src/main/java/br/com/lotus_projeto_integrador/lotus/HomeFragment.java
+++ app/src/main/java/br/com/lotus_projeto_integrador/lotus/HomeFragment.java
@@ -2,11 +2,16 @@ package br.com.lotus_projeto_integrador.lotus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class HomeFragment extends Fragment {
@@ -21,25 +26,24 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Button btnCamera = (Button) view.findViewById(R.id.btnCamera);
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
+        FloatingActionButton myFab = (FloatingActionButton)  view.findViewById(R.id.btnCamera);
+        myFab.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), CameraTestActivity.class);
                 startActivity(intent);
             }
         });
 
-        Button BtnLogin = (Button) view.findViewById(R.id.BtnLogin);
-        BtnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Pass the context and the Activity class you need to open from the Fragment Class, to the Intent
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
+        final ImageView splash = (ImageView)view.findViewById(R.id.imgLogo);
+        RotateAnimation anim = new RotateAnimation(0f, 350f, 15f, 15f);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setDuration(700);
 
-            }
-        });
+        splash.startAnimation(anim);
+
+// Later.. stop the animation
+        splash.setAnimation(null);
 
         return view;
     }
